29924997669980508437fbd8cb50715ef6523370
Dragomir-Alex <dragomario2k@gmail.com>
2023-01-19 22:13:53 +0200

    Added API client

---
+++ GameDataAppClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using GameDataAPIClient;
+
+namespace GameDataAppClient
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var client = new swaggerClient("https://localhost:7087", httpClient);
+
+            var results = await client.GetItemAsync(1);
+
+            Console.WriteLine(results.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
